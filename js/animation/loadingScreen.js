c3fe5371420522c65178e266185bb5a0a8c8ee74
Sedef Cakmak <cakmaksedef@gmail.com>
2022-08-19 05:10:02 -0400

    Tabler user-index sayfasina yerlestirildi, kredi gosteren dugme eklendi

--- js/animation/loadingScreen.js
+++ js/animation/loadingScreen.js
@@ -1,19 +1,19 @@
-const preloader = document.getElementById("preloader");
-const eventButtons = document.querySelectorAll(".site_name");
-const myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"), {
-  keyboard: false,
-});
+// const preloader = document.getElementById("preloader");
+// const eventButtons = document.querySelectorAll(".site_name");
+// const myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"), {
+//   keyboard: false,
+// });
 
-eventButtons.forEach((button) => {
-  button.addEventListener("click", playPreloader);
-});
+// eventButtons.forEach((button) => {
+//   button.addEventListener("click", playPreloader);
+// });
 
-function closePreloader() {
-  myModal.toggle();
-}
+// function closePreloader() {
+//   myModal.toggle();
+// }
 
-function playPreloader(e) {
-  e.preventDefault();
-  myModal.toggle();
-  setTimeout(closePreloader, 10000);
-}
+// function playPreloader(e) {
+//   e.preventDefault();
+//   myModal.toggle();
+//   setTimeout(closePreloader, 10000);
+// }

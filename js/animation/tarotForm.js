bbf52d014f136291ed862dc0573847acf62bf128
Sedef Cakmak <cakmaksedef@gmail.com>
2022-09-05 16:53:38 +0300

    iskambil, tarot, katina kartlari tamamlandi, hidden input ile secilen kartlar store ediliyor

--- js/animation/tarotForm.js
+++ js/animation/tarotForm.js
@@ -1,16 +1,16 @@
 const tarotOptions = document.querySelectorAll(".tarot-reading-option");
-const cards = document.querySelectorAll(".tarot-cards");
+const cards = document.querySelectorAll(".all-cards");
 const totalCards = document.querySelector(".total-cards");
-const selectCards = document.querySelector(".tarot-cards-remaining");
+const selectCards = document.querySelector(".all-cards-remaining");
 
-const wrapper = document.querySelector(".tarot-cards-wrapper");
-const selectCardsAlternative = document.querySelector(
-  ".tarot-cards-remaining-alternative"
-);
-const cardsSpread = document.querySelectorAll(".tarot-cards-alternative");
-const totalCardsAlternative = document.querySelector(
-  ".total-cards-alternative"
-);
+const wrapper = document.querySelector(".all-cards-wrapper");
+// const selectCardsAlternative = document.querySelector(
+//   ".all-cards-remaining-alternative"
+// );
+// const cardsSpread = document.querySelectorAll(".all-cards-alternative");
+// const totalCardsAlternative = document.querySelector(
+//   ".total-cards-alternative"
+// );
 
 const selectedCardsData = document.querySelector(
   'input[name$="selected-cards"]'
@@ -28,15 +28,20 @@ let remainingCards;
 let selectedCardsArray;
 
 // Kartlarin yerini rastgele belirle
+//Tarot kartlari icin ters ya da duz olma halini rastgele belirle
+
 function shuffleCards() {
   cards.forEach((card) => {
     if (card.classList.contains("selected")) {
       card.classList.remove("selected");
     }
-    const randomDegrees = [0, 90];
-    const randomDegree =
-      randomDegrees[Math.floor(Math.random() * randomDegrees.length)];
-    card.style.transform = `rotate(${randomDegree}deg)`;
+    if (card.classList.contains("tarot")) {
+      const reverseOrNot = ["reverse", "not-reverse"];
+      const random =
+        reverseOrNot[Math.floor(Math.random() * reverseOrNot.length)];
+      if (random === "reverse") card.classList.add("reverse");
+    }
+
     selectedCardsArray = [];
     remainingCards = 7;
     selectCards.textContent = remainingCards;
@@ -46,122 +51,133 @@ function shuffleCards() {
 }
 shuffleCards();
 
-cardsSpread.forEach((card) => {
-  let randomPosition = Math.floor(Math.random() * cardsSpread.length);
-  card.style.order = randomPosition;
-  cardShuffled = true;
-  spreadCards();
-});
-
 //Spread Alternative Cards
 
-totalCardsAlternative.textContent = cardsSpread.length;
+// cardsSpread.forEach((card) => {
+//   let randomPosition = Math.floor(Math.random() * cardsSpread.length);
+//   card.style.order = randomPosition;
+//   cardShuffled = true;
+//   spreadCards();
+// });
 
-function spreadCards() {
-  let leftPosition = 0;
-  let rotateDegree = -9;
-  let translateDegree = 0;
+// totalCardsAlternative.textContent = cardsSpread.length;
 
-  cardsSpread.forEach((card, index) => {
-    card.style.left = `${leftPosition}px`;
-    leftPosition +=
-      (wrapper.clientWidth - card.clientWidth) / cardsSpread.length;
-    card.style.transform = `rotate(${rotateDegree}deg)`;
-    rotateDegree += 0.25;
+// function spreadCards() {
+//   let leftPosition = 0;
+//   let rotateDegree = -9;
+//   let translateDegree = 0;
 
-    if (index <= 38) {
-      card.style.transform += `translateY(${translateDegree}rem)`;
-      translateDegree -= 0.012;
-    } else if (index > 38) {
-      card.style.transform += `translateY(${translateDegree}rem)`;
-      translateDegree += 0.012;
-    }
-  });
-}
+//   cardsSpread.forEach((card, index) => {
+//     card.style.left = `${leftPosition}px`;
+//     leftPosition +=
+//       (wrapper.clientWidth - card.clientWidth) / cardsSpread.length;
+//     card.style.transform = `rotate(${rotateDegree}deg)`;
+//     rotateDegree += 0.25;
 
-let res;
-window.onresize = function () {
-  if (res) {
-    clearTimeout(res);
-  }
-  res = setTimeout(spreadCards, 100);
-};
+//     if (index <= 38) {
+//       card.style.transform += `translateY(${translateDegree}rem)`;
+//       translateDegree -= 0.012;
+//     } else if (index > 38) {
+//       card.style.transform += `translateY(${translateDegree}rem)`;
+//       translateDegree += 0.012;
+//     }
+//   });
+// }
+
+// let res;
+// window.onresize = function () {
+//   if (res) {
+//     clearTimeout(res);
+//   }
+//   res = setTimeout(spreadCards, 100);
+// };
 
 //OnClick Animation
-
-cardsSpread.forEach((card) => {
-  if (cardShuffled) {
-    card.addEventListener("click", function () {
-      if (remainingCards > 0) {
-        if (card.classList.contains("selected-alternative")) {
-          card.classList.remove("selected-alternative");
-          remainingCards++;
-        } else {
-          card.classList.add("selected-alternative");
-          remainingCards--;
-        }
-      } else if (
-        remainingCards === 0 &&
-        card.classList.contains("selected-alternative")
-      ) {
-        card.classList.remove("selected-alternative");
-        remainingCards++;
-      }
-      selectCardsAlternative.textContent = remainingCards;
-    });
-  }
-});
 
 cards.forEach((card) => {
   card.addEventListener("click", function () {
     if (remainingCards > 0) {
       if (card.classList.contains("selected")) {
         card.classList.remove("selected");
-        selectedCardsArray = selectedCardsArray.filter((e) => e !== card.id);
+        selectedCardsArray = selectedCardsArray.filter(
+          (e) => e !== card.id && e !== `R${card.id}`
+        );
         remainingCards++;
       } else {
         card.classList.add("selected");
-        selectedCardsArray.push(card.id + "t");
+        card.classList.contains("reverse")
+          ? selectedCardsArray.push(`R${card.id}`)
+          : selectedCardsArray.push(`${card.id}`);
         remainingCards--;
       }
     } else if (remainingCards === 0 && card.classList.contains("selected")) {
       card.classList.remove("selected");
-      selectedCardsArray = selectedCardsArray.filter((e) => e !== card.id);
+      selectedCardsArray = selectedCardsArray.filter(
+        (e) => e !== card.id && e !== `R${card.id}`
+      );
       remainingCards++;
     }
+    console.log("array " + selectedCardsArray);
+    selectedCardsData.value = selectedCardsArray;
+    console.log("data " + selectedCardsArray);
     selectCards.textContent = remainingCards;
-    matchWithImages();
+
+    if (card.classList.contains("tarot")) {
+      cardType = "tarot";
+    } else if (card.classList.contains("katina")) {
+      cardType = "katina";
+    } else if (card.classList.contains("iskambil")) {
+      cardType = "iskambil";
+    }
+    matchWithImages(cardType);
   });
 });
 
-function matchWithImages() {
+function matchWithImages(cardType) {
   selectedCardsArray.forEach(function (value, index) {
     const finalImage = document.getElementById(`card${index}`);
-    finalImage.src = `../../assets/images/tarot-kartlari/${value}.png`;
-    if (value === `${value}t`) console.log("workinnngs");
+    if (value.includes("R")) {
+      let source = value.replace("R", "");
+      finalImage.src = `../../assets/images/${cardType}-kartlari/${source}.png`;
+      finalImage.classList.add("reverse-rotate");
+    } else {
+      finalImage.src = `../../assets/images/${cardType}-kartlari/${value}.png`;
+      finalImage.classList.remove("reverse-rotate");
+    }
   });
-  selectedCardsData.value = selectedCardsArray;
-  // console.log(selectedCardsData.value);
 }
-// function matchWithImages() {
-//   selectedCardsArray.forEach(function (value, index) {
-//     const randomDegrees = [0, 180];
-//     const randomDegree =
-//       randomDegrees[Math.floor(Math.random() * randomDegrees.length)];
-//     const finalImage = document.getElementById(`card${index}`);
-//     finalImage.src = `../../assets/images/tarot-kartlari/${value}.png`;
-//     finalImage.style.transform = `rotate(${randomDegree}deg)`;
-//   });
-//   selectedCardsData.value = selectedCardsArray;
-//   console.log(selectedCardsData.value);
-// }
 
 //Yorumcuya gonderilecek kisim
 
 //Gosterme amacli button
 document.querySelector(".show-div").addEventListener("click", () => {
-  document.querySelector(".display-tarot-cards").style.display = "block";
+  document.querySelector(".display-all-cards").style.display = "block";
 });
+
+// Alternative Versiyon icin
+
+// cardsSpread.forEach((card) => {
+//   if (cardShuffled) {
+//     card.addEventListener("click", function () {
+//       if (remainingCards > 0) {
+//         if (card.classList.contains("selected-alternative")) {
+//           card.classList.remove("selected-alternative");
+//           remainingCards++;
+//         } else {
+//           card.classList.add("selected-alternative");
+//           remainingCards--;
+//         }
+//       } else if (
+//         remainingCards === 0 &&
+//         card.classList.contains("selected-alternative")
+//       ) {
+//         card.classList.remove("selected-alternative");
+//         remainingCards++;
+//       }
+//       selectCardsAlternative.textContent = remainingCards;
+//     });
+//   }
+// });
 
 // Form Validasyon
 
@@ -189,7 +205,7 @@ document.querySelector(".show-div").addEventListener("click", () => {
           );
           notification.style.color = "red";
           document
-            .getElementById("tarot-cards-remaining-text")
+            .getElementById("all-cards-remaining-text")
             .appendChild(notification);
         } else {
           const myModal = new bootstrap.Modal(
